1fee40938136b59f34c6daee3ab0a190bf8a4308
Vitalii Lymar <vlm191997@gmail.com>
2020-06-02 13:57:51 +0300

    Attach buttons outlets and actions

--- 100DaysOfSwiftProject2/ViewController.swift
+++ 100DaysOfSwiftProject2/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func buttonTaped(_ sender: UIButton) {
+    }
+    
 
 }
 
